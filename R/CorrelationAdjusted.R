84afc579d9f0ec199e4d11454f63a2040acbe51d
Craig44 <craig.marsh@bigbluedata.co.nz>
2022-06-05 14:59:25 +1200

    UPD: Correlation

--- R/CorrelationAdjusted.R
+++ R/CorrelationAdjusted.R
@@ -7,7 +7,7 @@
 #' @importFrom raster Moran
 #' @return Spatial autocorrealtion variance estimator for the poopulation total
 #' 
-CorrelationAdjusted = function(y, sample_area, survey_area) {
+CorrelationAdjusted = function(y, survey_area) {
   if (class(y) != "RasterLayer")
     stop(paste0("y needs to be of class 'RasterLayer', please reformat it"))
 
@@ -16,9 +16,9 @@ CorrelationAdjusted = function(y, sample_area, survey_area) {
   result = 0.0;
   ## D'Orozia adjusted correlation
   rho_hat = Moran(y)
-  result = survey_area^2 * var(y$layer@data@values / sample_area) / length(y$layer@data@values)
+  var_est = var(y$layer@data@values) / length(y$layer@data@values)
   if (rho_hat > 0)
-    result = result * (1 + 2/log(rho_hat) + 2 / (1/rho_hat - 1))
+    var_est = var_est * (1 + 2/log(rho_hat) + 2 / (1/rho_hat - 1))
   
-  return(list(var_est = result, rho = rho_hat))
+  return(list(var_est = var_est, rho = rho_hat))
 }
