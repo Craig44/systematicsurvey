a114af24b2965acec624a82808d428e08e4bd9a5
Craig Marsh <Craig.Marsh@niwa.co.nz>
2022-06-27 10:24:34 +1200

    UPD: documentation

--- R/CorrelationAdjusted.R
+++ R/CorrelationAdjusted.R
@@ -1,18 +1,14 @@
 #' CorrelationAdjusted 
-#' @details a functon that evaluates the variacnce estimator for population total, with an adjustment based on the spatial autocorrelation, based on Morans I 
-#' @param y sample data of class "RasterLayer" see an example for how this is created.
-#' @param sample_area area of each sampling unit which has to be a single value i.e. sampling unit area are all the same
-#' @param survey_area scalar for survey area
+#' @details calculate the correlation adjusted variance estimator for the mean from \insertRef{d2003estimating;textual}{systematicsurvey}. This adjusts the simple random sampling estimator using an adjustment based on the spatial autocorrelation, based on Morans I 
+#' @param y sample data (assumed to be densities) of class "RasterLayer" see an example for how this is created.
 #' @export
 #' @importFrom raster Moran
+#' @importFrom Rdpack reprompt
 #' @return Spatial autocorrealtion variance estimator for the poopulation total
 #' 
 CorrelationAdjusted = function(y, survey_area) {
   if (class(y) != "RasterLayer")
     stop(paste0("y needs to be of class 'RasterLayer', please reformat it"))
-
-  if (class(sample_area) != "numeric" & length(sample_area) != 1) 
-    stop("CorrelationAdjusted: expects 'sample_area' to be a scalar, this is not the case, current form assumes equal area sampling units")
   result = 0.0;
   ## D'Orozia adjusted correlation
   rho_hat = Moran(y)
